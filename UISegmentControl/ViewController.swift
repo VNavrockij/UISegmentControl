eb502e3a1f7ca6217c567d81d5ec6800b2ea4f7e
VNavrockij <vitalik.nav@gmail.com>
2022-05-04 14:23:02 +0300

    feat: some logic

--- UISegmentControl/ViewController.swift
+++ UISegmentControl/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.isHidden = true
+        label.font = label.font.withSize(20)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
     }
 
-
+    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
+        
+        label.isHidden = false
+        
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            label.text = "The 1st segment is selected"
+            label.textColor = .red
+        case 1:
+            label.text = "The 2st segment is selected"
+            label.textColor = .blue
+        case 2:
+            label.text = "The 3rd segment is selected"
+            label.textColor = .darkGray
+        default:
+            label.text = "Not segement is selected"
+        }
+    }
+    
 }
 
