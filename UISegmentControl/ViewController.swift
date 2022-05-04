368865630d02ce3db5ba13c30f835a7d7b878b78
VNavrockij <vitalik.nav@gmail.com>
2022-05-04 14:13:15 +0300

    add: new task for segment contol

--- UISegmentControl/ViewController.swift
+++ UISegmentControl/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
